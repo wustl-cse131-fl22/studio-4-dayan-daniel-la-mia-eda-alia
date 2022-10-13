c28aa739c3e679d29a080380f776128586d01453
Dayan-Parker <112508750+Dayan-Parker@users.noreply.github.com>
2022-10-13 13:54:38 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,20 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		
+		double parameterOne = 0.5;
+		double parameterTwo = 0.5;
+		double parameterThree = 0.35;
+		double parameterFour = 0.2;
+		
+		StdDraw.setPenColor(255, 109,182);
+		StdDraw.filledRectangle(parameterOne,parameterTwo, parameterThree, parameterFour);
+		
+		StdDraw.setPenColor(0, 0,255);
+		StdDraw.filledRectangle(parameterOne,parameterTwo, parameterThree/2, parameterFour/2);
+		
+		StdDraw.setPenColor(0, 255,0);
+		StdDraw.filledEllipse(parameterOne,parameterTwo, parameterThree/3, parameterFour/3);
 	}
 }
